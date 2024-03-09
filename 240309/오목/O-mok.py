3e766b7a8dbd001f12cdae0c607546c02a7fc07b
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-09 19:08:30 +0900

    Title: 오목, Time: 134ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240309/오목/O-mok.py
@@ -0,0 +1,40 @@
+import sys
+
+si = sys.stdin.readline
+
+arr = [
+    list(map(int, si().split()))
+    for _ in range(19)
+]
+
+# 가로 검사
+for i in range(19):
+    for j in range(15):
+        if arr[i][j] == 1 and arr[i][j + 1] == 1 and arr[i][j + 2] == 1 and arr[i][j + 3] == 1 and arr[i][j + 4] == 1:
+            print(1)
+            print(i + 1, j + 3)
+
+        if arr[i][j] == 2 and arr[i][j + 1] == 2 and arr[i][j + 2] == 2 and arr[i][j + 3] == 2 and arr[i][j + 4] == 2:
+            print(2)
+            print(i + 1, j + 3)
+# 세로 검사
+for i in range(15):
+    for j in range(19):
+        if arr[i][j] == 1 and arr[i + 1][j] == 1 and arr[i + 2][j] == 1 and arr[i + 3][j] == 1 and arr[i + 4][j] == 1:
+            print(1)
+            print(i + 3, j + 1)
+
+        if arr[i][j] == 2 and arr[i + 1][j] == 2 and arr[i + 2][j] == 2 and arr[i + 3][j] == 2 and arr[i + 4][j] == 2:
+            print(2)
+            print(i + 3, j + 1)
+
+# 대각선 검사 
+for i in range(15):
+    for j in range(15):
+        if arr[i][j] == 1 and arr[i + 1][j + 1] == 1 and arr[i + 2][j + 2] == 1 and arr[i + 3][j + 3] == 1 and arr[i + 4][j + 4] == 1:
+            print(1)
+            print(i + 3, j + 3)
+
+        if arr[i][j] == 2 and arr[i + 1][j + 1] == 2 and arr[i + 2][j + 2] == 2 and arr[i + 3][j + 3] == 2 and arr[i + 4][j + 4] == 2:
+            print(2)
+            print(i + 3, j + 3)
