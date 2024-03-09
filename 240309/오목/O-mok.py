395843e9ee80ada27637cb12af11d457f90587b2
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-09 19:12:29 +0900

    Title: 오목, Time: 137ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240309/오목/O-mok.py
+++ 240309/오목/O-mok.py
@@ -28,7 +28,7 @@ for i in range(15):
             print(2)
             print(i + 3, j + 1)
 
-# 대각선 검사 
+# 대각선 검사(우하향)
 for i in range(15):
     for j in range(15):
         if arr[i][j] == 1 and arr[i + 1][j + 1] == 1 and arr[i + 2][j + 2] == 1 and arr[i + 3][j + 3] == 1 and arr[i + 4][j + 4] == 1:
@@ -38,3 +38,14 @@ for i in range(15):
         if arr[i][j] == 2 and arr[i + 1][j + 1] == 2 and arr[i + 2][j + 2] == 2 and arr[i + 3][j + 3] == 2 and arr[i + 4][j + 4] == 2:
             print(2)
             print(i + 3, j + 3)
+
+# 대각선 검사(우상향)
+for i in range(5, 19):
+    for j in range(15):
+        if arr[i][j] == 1 and arr[i - 1][j + 1] == 1 and arr[i - 2][j + 2] == 1 and arr[i - 3][j + 3] == 1 and arr[i - 4][j + 4] == 1:
+            print(1)
+            print(i - 1, j + 3)
+
+        if arr[i][j] == 2 and arr[i - 1][j + 1] == 2 and arr[i - 2][j + 2] == 2 and arr[i - 3][j - 3] == 2 and arr[i - 4][j + 4] == 2:
+            print(2)
+            print(i - 1, j + 3)
