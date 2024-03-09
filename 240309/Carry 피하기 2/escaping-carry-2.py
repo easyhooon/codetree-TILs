cc74f56cfe51d2d84c9c133df56615583fa7c97c
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-09 17:44:44 +0900

    Title: Carry 피하기 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240309/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,36 @@
+import sys
+
+si = sys.stdin.readline 
+
+n = int(si())
+arr = []
+
+for _ in range(n):
+    temp = int(si())
+    arr.append(temp)
+
+max_sum = -sys.maxsize
+
+# 캐리가 발생하지 않으면서 최대 합 
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            # 각 자리의 수의 합이 10이 넘지 않는지 검사 
+            max_num = max(arr[i], arr[j], arr[k])
+            max_len = len(str(max_num))
+            # sum_val = arr[i] + arr[j] + arr[k]
+            flag = True
+            sum_val = 0
+            for l in range(1, max_len + 1):
+                if l < len(str(arr[i])):
+                    sum_val += int(str(arr[i])[l])
+                if l < len(str(arr[j])):
+                    sum_val += int(str(arr[j])[l])
+                if l < len(str(arr[k])):
+                    sum_val += int(str(arr[k])[l])
+                if sum_val >= 10:
+                    flag = False
+                    break
+            if (flag):
+                max_sum = max(max_sum, arr[i] + arr[j] + arr[k])
+print(max_sum)
