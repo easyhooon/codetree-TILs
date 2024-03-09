17ea72f397e7072d75c591e2ade423dccf73a4ec
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-09 23:27:02 +0900

    Title: 구간 중 최대 합, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240309/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,17 @@
+import sys
+
+si = sys.stdin.readline
+
+n, k = map(int, si().split())
+
+arr = list(map(int, si().split()))
+
+max_sum = -sys.maxsize
+
+for i in range(n - k):
+    sum_val = arr[i]
+    for j in range(1, k):
+        sum_val += arr[i + j]
+    max_sum = max(max_sum, sum_val)
+
+print(max_sum)
