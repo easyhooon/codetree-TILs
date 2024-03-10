e11b0d3ec0bbf969e81634d111e9a6b5f94295bf
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-10 10:40:26 +0900

    Title: 아름다운 수열 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/아름다운 수열 2/beautiful-sequence-2.py
+++ 240310/아름다운 수열 2/beautiful-sequence-2.py
@@ -13,6 +13,7 @@ for i in range(n - len(b_arr) + 1):
     for j in range(i, i + len(b_arr)):
         temp_arr.append(a_arr[j])
     temp_arr.sort()
+    b_arr.sort()
     if b_arr == temp_arr:
         cnt += 1
 
