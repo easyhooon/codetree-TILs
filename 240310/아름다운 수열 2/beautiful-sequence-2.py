dd933c992776ce27efa19d99b8bc2f1ba3ebbfa8
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-10 10:39:25 +0900

    Title: 아름다운 수열 2, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240310/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,19 @@
+import sys
+
+si = sys.stdin.readline
+
+n, m = map(int, si().split())
+
+a_arr = list(map(int, si().split()))
+b_arr = list(map(int, si().split()))
+
+cnt = 0
+for i in range(n - len(b_arr) + 1):
+    temp_arr = []
+    for j in range(i, i + len(b_arr)):
+        temp_arr.append(a_arr[j])
+    temp_arr.sort()
+    if b_arr == temp_arr:
+        cnt += 1
+
+print(cnt)
