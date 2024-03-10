7009aeed77291f9560f4e89c22104e6821ad5be7
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-10 21:43:58 +0900

    Title: 바구니 안의 사탕 2, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240310/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240310/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -7,6 +7,7 @@ temp_arr = []
 max_coord = -sys.maxsize
 
 #  단, 같은 위치에 여러 바구니가 놓여 있는 것이 가능함에 유의합니다.
+# k 가 [1, max_coord] 를 초과할 수 있다. -> 전체 범위 
 for _ in range(n):
     cnt, coord = map(int, si().split())
     max_coord = max(max_coord, coord)
@@ -19,11 +20,17 @@ for coord, cnt in temp_arr:
 
 max_sum = -sys.maxsize
 
-for i in range(1, max_coord - 2 * k + 1):
+if 2 * k >= max_coord:
     sum_val = 0
-    for j in range(2 * k + 1):
-        sum_val += arr[i + j]
+    for i in range(1, max_coord + 1):
+        sum_val += arr[i]
+    print(sum_val)
+else:
+    for i in range(1, max_coord - 2 * k + 1):
+        sum_val = 0
+        for j in range(2 * k + 1):
+            sum_val += arr[i + j]
 
-    max_sum = max(max_sum, sum_val)
+        max_sum = max(max_sum, sum_val)
 
-print(max_sum)
+    print(max_sum)
