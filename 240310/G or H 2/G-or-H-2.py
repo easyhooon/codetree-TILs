106621e6799564d61258710c517eb1c2c8775d0e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-10 23:35:45 +0900

    Title: G or H 2, Time: 171ms, Memory: 25MB, Status: Passed - CodeTree

--- 240310/G or H 2/G-or-H-2.py
+++ 240310/G or H 2/G-or-H-2.py
@@ -22,6 +22,23 @@ for coord, alphabet in temp:
 
 # 투포인터 풀듯이 풀어보자
 # 근데 어디서 만나는지 그런게 중요한게 아닌데,,
-start = 0
-end = max_coord
-while start <= end:
+max_len = 0
+for i in range(max_coord + 1):
+    for j in range(i + 1, max_coord + 1):
+        if arr[i] == 0 or arr[j] == 0:
+            continue
+
+        cnt_g = 0
+        cnt_h = 0
+
+        for k in range(i, j + 1):
+            if arr[k] == 1:
+                cnt_g += 1
+            if arr[k] == 2:
+                cnt_h += 1
+
+        if cnt_g == 0 or cnt_h == 0 or cnt_g == cnt_h:
+            len_val = j - i
+            max_len = max(max_len, len_val)
+
+print(max_len)
