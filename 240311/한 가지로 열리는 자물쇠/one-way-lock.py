5ba3fb2886d03c1a4aec85d3aa59d9c3ae38b7ce
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-11 14:11:17 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 187ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240311/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -0,0 +1,14 @@
+import sys
+
+si = sys.stdin.readline
+
+n = int(si())
+arr = list(map(int, si().split()))
+
+cnt = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        for k in range(1, n + 1):
+            if abs(i - arr[0]) <= 2 or abs(j - arr[1]) <= 2 or abs(k - arr[2]) <= 2:
+                cnt += 1
+print(cnt)
