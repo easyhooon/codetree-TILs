2f20128af699d0b6be5eea981747e233d89986db
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-11 16:14:51 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 162ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240311/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240311/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -15,7 +15,7 @@ else:
     for i in range(1, n - 2):
         for j in range(i + 1, n - 1):
             for k in range(j + 1, n):
-                if (abs(i - a_arr[0]) <= 2 and abs(j - a_arr[1] <= 2) and abs(k - a_arr[2]) <= 2) and (abs(i - b_arr[0]) <= 2 and abs(j - b_arr[1]) <= 2 and abs(k - b_arr[2]) <= 2):
+                if ((abs(i - a_arr[0]) <= 2 or abs(i - a_arr[0]) <= n - 2) and (abs(j - a_arr[1]) <= 2 or abs(j - a_arr[1]) <= n - 2) and (abs(k - a_arr[2]) <= 2 or abs(k - a_arr[2]) <= n - 2)) or ((abs(i - b_arr[0]) <= 2 or abs(i - b_arr[0]) <= n - 2) and (abs(j - b_arr[1]) <= 2 or abs(j - b_arr[1]) <= n - 2) and (abs(k - b_arr[2]) <= 2 or abs(k - b_arr[2]) <= n - 2)):
                     # print(i, j, k)
                     cnt += 1
 
