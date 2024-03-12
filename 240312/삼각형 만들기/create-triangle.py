ad2e340888050ce20611f84e558606925b5f5f9e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-12 10:48:11 +0900

    Title: 삼각형 만들기, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240312/삼각형 만들기/create-triangle.py
+++ 240312/삼각형 만들기/create-triangle.py
@@ -11,20 +11,22 @@ arr = [
 
 max_area = -sys.maxsize
 
-for i in range(n):
-    max_x = 0
-    min_x = sys.maxsize
-    max_y = 0
-    min_y = sys.maxsize
-    for j in range(n):
-        if i == j:
-            continue
 
-        max_x = max(max_x, arr[j][0])
-        min_x = min(min_x, arr[j][0])
-        max_y = max(max_y, arr[j][1])
-        min_y = min(min_y, arr[j][1])
-
-    max_area = max(max_area, (max_x - min_x) * (max_y - min_y))
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            # 3개의 점을 골랐을 때 각 변이 x, y 축에 평행하는지 검사 필요
+            # x 축과 평행 -> y 좌표가 같다
+            # y 축과 평행 -> x 좌표가 같다
+            x_set = set()
+            x_set.add(arr[i][0])
+            x_set.add(arr[j][0])
+            x_set.add(arr[k][0])
+            y_set = set() 
+            y_set.add(arr[i][1])
+            y_set.add(arr[j][1])
+            y_set.add(arr[k][1])
+            if len(x_set) == 2 and len(y_set) == 2:
+                max_area = max(max_area, arr[i][0] * arr[j][1] + arr[j][0] * arr[k][1] + arr[k][0] * arr[i][1] - arr[j][0] * arr[i][1] - arr[k][0] * arr[j][1] - arr[i][0] * arr[k][1])
 
 print(max_area)
