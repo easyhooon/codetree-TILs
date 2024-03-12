77eed48fa200d028e33f4a100e9543034b6d4687
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-12 20:27:07 +0900

    Title: 등차수열, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240312/등차수열/arithmetic-sequence.py
@@ -0,0 +1,21 @@
+import sys
+
+si = sys.stdin.readline
+
+n = int(si())
+
+arr = list(map(int, si().split()))
+arr.sort()
+start = arr[0]
+end = arr[len(arr) - 1]
+
+cnt = 0
+for i in range(start ,end + 1):
+    for j in range(n - 1):
+        for k in range(1, n):
+            if j + k >= n:
+                continue
+            if abs(i - arr[j]) == abs(i - arr[j + k]):
+                cnt += 1
+
+print(cnt)
