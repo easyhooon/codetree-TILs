5e64996da75d16661f36f7a1a204210e707e62e0
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-13 13:35:53 +0900

    Title: 숫자 2배 후 하나 제거하기, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240313/숫자 2배 후 하나 제거하기/multiply-two-and-remove-one-number.py
@@ -0,0 +1,26 @@
+import sys
+
+si = sys.stdin.readline
+
+n = int(si())
+arr = list(map(int, si().split()))
+
+min_diff = sys.maxsize
+for i in range(n):
+    arr[i] *= 2
+    
+    for j in range(n):
+        remaining_arr = []
+        for k in range(n):
+            if k != j:
+                remaining_arr.append(arr[k])
+
+        sum_diff = 0
+        for k in range(n - 2):
+            sum_diff += abs(remaining_arr[k + 1] - remaining_arr[k])
+
+        min_diff = min(min_diff, sum_diff)
+
+    arr[i] //= 2
+
+print(min_diff)
