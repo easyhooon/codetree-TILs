89794c0c01d8f8d7a3487a2a1ce572b6bf8c8775
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-13 21:29:58 +0900

    Title: 3개의 선 2, Time: 148ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240313/3개의 선 2/three-lines-2.py
@@ -0,0 +1,43 @@
+import sys
+
+si = sys.stdin.readline
+
+n = int(si())
+
+arr = [
+    list(map(int, si().split()))
+    for _ in range(n)
+]
+
+answer = 0
+for i in range(11):
+    for j in range(11):
+        for k in range(11):
+            success = True
+            for x, y in arr:
+                if x == i or x == j or x == k:
+                    continue
+                
+                success = False
+            if success:
+                answer = 1
+
+            success = True 
+            for x, y in arr:
+                if x == i or x == j or y == k:
+                    continue
+                
+                success = False
+            if success:
+                answer = 1
+
+            success = True
+            for x, y in arr:
+                if x == i or y == j or y == k:
+                    continue
+
+                success = False
+            if success:
+                answer = 1
+
+print(answer)
