55bb1bb46a27250b0eb879c8d057ad3bd2ad0e9f
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-13 13:58:06 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240313/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.py
@@ -0,0 +1,18 @@
+import sys
+
+si = sys.stdin.readline 
+
+a, b, c = map(int, si().split())
+
+a_max_cnt = c // a
+b_max_cnt = c // b
+
+max_sum = -sys.maxsize
+for i in range(1, a_max_cnt):
+    for j in range(1, b_max_cnt):
+        if a * i + b * j >= c:
+            break
+
+        max_sum = max(max_sum, a * i + b * j)
+
+print(max_sum)
