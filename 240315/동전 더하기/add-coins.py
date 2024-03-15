72469781c1740ba2ca02b4a05300e7a05cf2252d
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-15 16:41:34 +0900

    Title: 동전 더하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240315/동전 더하기/add-coins.py
@@ -0,0 +1,20 @@
+import sys
+
+si = sys.stdin.readline
+
+n, k = map(int, si().split())
+
+arr = [
+    list(int(si()) 
+    for _ in range(n))
+]
+
+cnt = 0
+for i in range(n - 1, -1, -1):
+    if k % arr[i] == 0:
+        cnt += k // arr[i]
+        print(cnt)
+        exit()
+    else:
+        cnt += k // arr[i]
+        k %= arr[i]
