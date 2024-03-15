b1e463a8df61035d0a8ef663f2431ba844415166
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-15 18:05:27 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 175ms, Memory: 32MB, Status: Passed - CodeTree

--- 240315/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 240315/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -11,6 +11,8 @@ last = 0
 for i in range(n):
     sum_val = last + arr[i]
     if sum_val < 0:
+        # 주어진 모든 수가 음수일 수 도 있음
+        max_sum = max(max_sum, sum_val)
         last = 0
     else:
         last = sum_val 
