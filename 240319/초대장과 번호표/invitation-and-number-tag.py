cb1a16d267c36ed1ac990e5bdd83e34b812b5d1c
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-19 17:03:38 +0900

    Title: 초대장과 번호표, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240319/초대장과 번호표/invitation-and-number-tag.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+si = sys.stdin.readline
+
+n, g = map(int, si().split())
+
+arr = [
+    list(map(int, si().split()))
+    for _ in range(g)
+]
+
+invited = [False] * n
+# 각 그룹마다 초대장을 받지 못한 사람들을 관리
+groups = [set() for _ in range(g)]
+people_groups = [[] for _ in range(n)]
+
+q = deque()
+answer = 0
+
+for i in range(g):
+    nums = list(map(int, si().split()))[1:]
+    for num in nums:
+        num -= 1
+        groups[i].add(num)
+        people_groups[num].append(i)
+
+q.append(0)
+invited[0] = True
+
+while(q):
+    x = q.popleft()
+    answer += 1
+
+    for g_num in people_groups[x]:
+        groups[g_num].remove(x)
+        if len(groups[g_num]) == 1:
+            p_num = list(groups[g_num])[0]
+            if not invited[p_num]:
+                invited[p_num] = True
+                q.append(p_num)
+
+print(answer)
