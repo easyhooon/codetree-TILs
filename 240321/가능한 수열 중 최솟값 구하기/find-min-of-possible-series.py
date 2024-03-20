c7d58d4cf1fbd5a1598a7fb105f699079611bf1e
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2024-03-21 02:00:31 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 5016ms, Memory: 29MB, Status: Time Limit Exceed - CodeTree

---
+++ 240321/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.py
@@ -0,0 +1,43 @@
+import sys
+
+si = sys.stdin.readline 
+
+n = int(si())
+numbers = [4, 5, 6]
+
+series = []
+
+
+def is_possible_series():
+    for idx in range(len(series)):
+        length = 1
+        while True:
+            start1, end1 = idx, idx + length - 1
+            start2, end2 = end1 + 1, (end1 + 1) + length - 1
+
+            if end2 >= len(series):
+                break
+
+            if series[start1:end1 + 1] == series[start2:end2 + 1]:
+                return False
+            
+            length += 1
+    
+    return True
+
+def find_min_series(cnt):
+    if cnt == n:
+        if not is_possible_series():
+            return
+        
+        # 가장 먼저 나오는 조합을 출력하고 프로그램 종료 
+        for elem in series:
+            print(elem, end = "")
+        sys.exit(0)
+
+    for number in numbers:
+        series.append(number)
+        find_min_series(cnt + 1)
+        series.pop()
+
+find_min_series(0)
